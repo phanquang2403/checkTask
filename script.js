07d8871ab0fb1df6d444cf10a4c14329c9aa5d54
Phan Quang <quang.phan@bnksolution.com>
2022-05-08 03:11:19 +0700

    demo logic run check

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const section1 = document.querySelector("#section1");
 const section2 = document.querySelector("#section2");
-
-const locationSection1 = section1.offsetTop;
-const locationSection2 = section2.offsetTop;
+const section3 = document.querySelector("#section3");
 
 const checkChildrenSectionAll = (selectNumber) => {
   for (const key in selectNumber) {
@@ -14,16 +12,25 @@ const checkChildrenSectionAll = (selectNumber) => {
   }
 };
 
-const checkSection = () => {
-  const selectNumber = section1.children;
+const checkSection = (sectionStart, sectionEnd = null) => {
+  const selectNumber = sectionStart.children;
+  const locationEnd = sectionEnd
+    ? window.pageYOffset + sectionEnd.getBoundingClientRect().top
+    : "";
   for (const key in selectNumber) {
     if (selectNumber.hasOwnProperty(key)) {
       let elementClick = selectNumber[key];
       elementClick.addEventListener("click", () => {
         checkChildrenSectionAll(selectNumber);
         elementClick.classList.add("no-active");
+        locationEnd != ""
+          ? (document.documentElement.scrollTop = locationEnd)
+          : null;
       });
     }
   }
 };
-checkSection();
+
+checkSection(section1, section2);
+checkSection(section2, section3);
+checkSection(section3);
